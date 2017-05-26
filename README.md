2600bd38f72e4792151ca9fb58b2ae23c162b844
stutiredboy <stutiredboy@gmail.com>
2017-05-26 11:57:57 +0800

    update readme

--- README.md
+++ README.md
@@ -7,3 +7,36 @@ Forked from: https://github.com/gophergala/dnsp
     go get github.com/stutiredboy/ddns
     cd ${GOPATH}/stutiredboy/ddns/cmd/ddns
     go build
+
+## Configurations
+
+```
+{
+	"ConnectTimeout": 500,
+	"Debug": true,
+	"Backends": {
+		"0": "127.0.0.1:6379",
+		"1": "127.0.0.1:6379"
+	},
+	"StatsPeriod": 60,
+	"StatsFile": "/home/tiredboy/ddns.stats",
+	"NameServers": ["8.8.8.8", "8.8.4.4"],
+	"ReadTimeout": 500,
+	"ChanNum": 4,
+	"PoolNum": 5,
+	"Listen": "127.0.0.1:5353"
+}
+```
+
+Configuration | Description
+------ | ------
+NameServers | name servers the DNS query forward to, format: address:port, default port is 53
+Listen | UDP Listen address:port
+StatsFile | stats file, absolute path
+StatsPeriod | ddns dump stats periodically(seconds)
+Backends | redis server
+ConnectTimeout | timeout for connecting to redis server, Millisecond
+ReadTimeout | timeout for read/write to redis server, Millisecond
+ChanNum | concurrency numbers for writing to redis
+PoolNum | redis connection pool, must little greater than *ChanNum*
+Debug | verbose output
---
+++ configurations.go
@@ -0,0 +1,60 @@
+package ddns
+
+import (
+	"net"
+	"errors"
+	"strings"
+)
+
+// Configurations for server
+type Configurations struct {
+    // host:port
+    Listen string
+    // [host1:port1, host2:port2]
+    NameServers []string
+    // Seconds, dump stats to StatsFile periodically
+    StatsPeriod int
+    StatsFile string
+    Backends map[int]string
+    // backend connection pool numbers
+    PoolNum int
+    // channel numbers, must little than PoolNum * len(Backends)
+    ChanNum int
+    // millseconds
+    ConnectTimeout int
+    // millseconds
+    ReadTimeout int
+    Debug bool
+}
+
+func (c *Configurations) validate() error {
+	if !strings.Contains(c.Listen, ":") {
+		c.Listen += ":53"
+	}
+	if l := len(c.Listen); l >= 4 && c.Listen[l-4:] == ":dns" {
+		c.Listen = c.Listen[:l-4] + ":53"
+	}
+	if c.Listen[0] == ':' {
+		c.Listen= "0.0.0.0" + c.Listen
+	}
+
+	for i, res := range c.NameServers {
+		if !strings.Contains(res, ":") {
+			res += ":53"
+		}
+		addr, err := net.ResolveUDPAddr("udp", res)
+		if err != nil {
+			return err
+		}
+		c.NameServers[i] = addr.String()
+	}
+	/* ensure have full coverage index */
+	for i := 0 ; i < len(c.Backends) ; i++ {
+		_, ok := c.Backends[i]
+		if ok == false {
+			err := errors.New("Wrong backends hash set, hash should full converage [0, len_of_Backends)")
+			return err
+		}
+	}
+	return nil
+}

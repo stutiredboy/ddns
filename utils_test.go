871dd164d21cfbc3c364e2162e4bf66de5e83747
stutiredboy <stutiredboy@gmail.com>
2017-06-09 13:39:57 +0800

    update test case

--- utils_test.go
+++ utils_test.go
@@ -5,9 +5,11 @@ import (
 )
 
 func Test_backendHas(t *testing.T) {
-	hash := backendHash("www.163.com")
+	//hash := backendHash("www.163.com")
+	hash := backendHash("Hello234")
 	/* must compatible with Python zlib.crc32('www.163.com') or binascii.crc32('www.163.com') */
 	if (hash != 832174588) {
+		t.Error("%s", hash)
 		t.Error("test func *backendHash* error")
 	}
 }
